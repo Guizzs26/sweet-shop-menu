ab13d6c6d48c47baf25981f82fb5716122cf39f5
Guilherme <guiteixeira1926@gmail.com>
2024-04-30 10:01:00 -0400

    fix: footer modification

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,7 @@ export default function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
-  // const isOpen = hour >= openHour && hour <= closeHour;
-  const isOpen = true;
+  const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
